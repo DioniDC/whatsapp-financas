8b152ca8a7c0de0c43ed4376d505e9fcb973b59d
Dioni Dias <dionidias_01@hotmail.com>
2025-05-19 18:54:12 -0300

    Evrificar se para de vir mensagem no email

--- example.js
+++ example.js
@@ -9,7 +9,7 @@ const tiposValidos = ['chat', 'ptt'];
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: false }
+    puppeteer: { headless: true }
 });
 
 client.initialize();
